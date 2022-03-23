ee61d6630b67d7b5c10d1f033bd0fb35eed867b8
lilynxi <lisa.mueller47@gmail.com>
2022-03-23 11:50:41 +0100

    add 404 page*

---
+++ 404.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+
+<div class="posts">
+
+	<div class="post">
+	
+		<div class="post-inner">
+							
+			<div class="post-header">
+					
+				<h1 class="post-title"><?php _e( 'Fehler', 'wilson' ); ?></h1>
+				
+			</div><!-- .post-header -->
+																		
+			<div class="post-content">
+							
+				<p><?php _e( "Diese Seite existiert nicht.", "wilson" ); ?></p>
+				
+				
+			</div><!-- .post-content -->
+		
+		</div><!-- .post-inner -->
+															
+	</div><!-- .post -->
+
+</div><!-- .posts -->
+
+<?php get_footer(); ?>
